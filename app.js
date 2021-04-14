25d51f7305dbc4a5b20ea6e5532c06226a9ccd83
onemo <wonmotime1@naver.com>
2021-04-14 22:11:20 +0900

    [ADD] users, login router / users, login controller

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
 
 const app = express();
 
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/auth/login", loginRouter);
 
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ controller/login.controller.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const { email, name } = req.body;
+    const currentUser = await User.findOne({ email });
+
+    if (!currentUser) await User.create(req.body);
+
+    const token = jwt.sign({ email, name }, process.env.JWT_SECRET);
+
+    res
+      .cookie("authToken", token)
+      .status(201)
+      .json({ result: "success", message: "로그인에 성공했습니다" });
+  } catch (err) {
+    next(createError(500, "로그인에 실패했습니다"));
+  }
+};
