61a1194b9e4851bc1521bb6186774eb0cd79da48
onemo <wonmotime1@naver.com>
2021-04-14 21:16:47 +0900

    [ADD] error handling in app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render("error");
+  res.json({ result: false, message: err.message });
 });
 
 module.exports = app;
