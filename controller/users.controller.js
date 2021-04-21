d63dfe8c79c46ee85c4ce83a572e370b2b44ab13
onemo <wonmotime1@naver.ccom>
2021-04-21 13:57:14 +0900

    [PATCH] users controller

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -1,14 +1,12 @@
 const createError = require("http-errors");
+const User = require("../models/User");
 
 module.exports.getAll = async (req, res, next) => {
   try {
     const users = await User.find().sort({ winningPoint: -1 });
 
     res
-      .status(200)
-      .json({
-        users
-      });
+      .json(users);
   } catch(err) {
     next(createError(500, err.message));
   }
